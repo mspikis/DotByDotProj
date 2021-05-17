3d727745a75f87f018b57dd0979bd3ba64764c1b
Michail Skalistiris Pikis <michaelskalistirispikis@gmail.com>
2021-05-17 11:45:27 +0300

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 7539ff04e9ace4cb5ddc8d6ba9232a7135a46472.

--- DotByDotProj/Controllers/HomeController.cs
+++ DotByDotProj/Controllers/HomeController.cs
@@ -1,18 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
 
-<<<<<<< HEAD
 namespace DotByDotProj.Controllers
-=======
-namespace MvcMovie.Controllers
->>>>>>> master
 {
     public class HomeController : Controller
     {
         // 
         // GET: /HelloWorld/
 
-<<<<<<< HEAD
         public IActionResult Index()
         {
             return View();
@@ -24,16 +19,6 @@ namespace MvcMovie.Controllers
             ViewData["NumTimes"] = numTimes;
 
             return View();
-=======
-        public string Index()
-        {
-            return "This is my default action...";
-        }
-
-        public string Welcome(string name, int ID = 1)
-        {
-            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
->>>>>>> master
         }
     }
 }
