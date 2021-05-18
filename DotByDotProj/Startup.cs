af253e9b7ab18433253078bd00384632deb2c7f0
Michail Skalistiris Pikis <michaelskalistirispikis@gmail.com>
2021-05-18 16:22:47 +0300

    Add a model

--- DotByDotProj/Startup.cs
+++ DotByDotProj/Startup.cs
@@ -8,22 +8,42 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using DotByDotProj.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace DotByDotProj
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        ppublic Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
+            Environment = env;
             Configuration = configuration;
         }
 
+
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<ProductListContext>(options =>
+            {
+                var connectionString = Configuration.GetConnectionString("ProductListContext");
+
+                if (Environment.IsDevelopment())
+                {
+                    options.UseSqlite(connectionString);
+                }
+                else
+                {
+                    options.UseSqlServer(connectionString);
+                }
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
